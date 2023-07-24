cec65aa7c69c592fdc2d85de254539fb466ce19d
Hashira0h <139925219+Hashira0h@users.noreply.github.com>
2023-07-24 08:01:26 -0700

    Delete main-menu.js

--- plugins/main-menu.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems })  => m.reply(`
-لسه ما انضافت
-`.trim())
-handler.help = ['allmenu']
-handler.tags = ['main']
-handler.command = ['ا', 'اوامر'] 
-
-export default handler
