74c0c8a0aa2ccf68fcf92fe7e1fe3c906197c17d
Hashira0h <139925219+Hashira0h@users.noreply.github.com>
2023-07-24 08:02:44 -0700

    Create قايمة الاستمارات.js

---
+++ plugins/قايمة الاستمارات.js
@@ -0,0 +1,9 @@
+let handler = m => m
+handler.all = async function (m) {
+let chat = global.db.data.chats[m.chat] 
+     if (/^استمارات$/i.test(m.text) ) { //sem prefixo
+    conn.reply(m.chat, `هضيفها اهو`, m) //wm, null, [['Menu', '#menu']], m) botones :V
+
+} 
+return !0 }
+export default handler
